da0aa457f1cca717757f59a18589734a34ebc01a
Matthew Oshimo <moshimo90696@gmail.com>
2023-08-14 22:37:18 -0700

    got openAI to work

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,52 +1,98 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+'use client'
 
-const inter = Inter({ subsets: ['latin'] })
+import Image from 'next/image'
+import React, { useRef } from 'react'
+
+interface Conversation {
+  role: string
+  content: string
+}
 
 export default function Home() {
+
+  const [value, setValue] = React.useState<string>("")
+  const [hide, setHide] = React.useState<string>("hidden")
+  const [prompter, setPrompter] = React.useState<string>("")
+  const [conversation, setConversation] = React.useState<Conversation[]>([])
+  const inputRef = useRef<HTMLInputElement>(null)
+
+  const handleInput = React.useCallback(
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      setValue(e.target.value)
+    },
+    []
+  )
+  const handleInput2 = React.useCallback(
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      setPrompter(e.target.value)
+    },[]
+  )
+
+
+  const handleKeyDown = async (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      const chatHistory = [...conversation, { role: "user", content: 'Based on the word "' + value + '", give me a prompt for you that clarifies in at least 3 sentences: Role, Style, and Goal.' }]
+
+      const response = await fetch("/api/OpenAI", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ messages: chatHistory }),
+      })
+
+      const data = await response.json()
+      setValue("")
+      setConversation([
+        ...chatHistory,
+        { role: "assistant", content: data.result.choices[0].message.content },
+      ])
+
+
+      setPrompter((data.result.choices[0].message.content))
+
+
+      setHide("")
+    }
+  }
+
+  const handleKeyDown2 = async () => {
+      const chatHistory = [...conversation, { role: "user", content: 'Behave in a manner based on this prompt: "' + value + '".'} ]
+ 
+      const response = await fetch("/api/OpenAI", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ messages: chatHistory }),
+      })
+
+      const data = await response.json()
+      setValue("")
+      setConversation([
+        ...chatHistory,
+        { role: "assistant", content: data.result.choices[0].message.content },
+      ])
+
+      setPrompter((data.result.choices[0].message.content))
+      
+      setHide("")
+  }
+
+  const handleRefresh = () => {
+    inputRef.current?.focus()
+    console.log("pressed")
+    setValue("")
+    setConversation([])
+  }
+
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+    <main className="flex h-screen min-w-900 flex-col content-start">
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
+      {/* <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
         <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
+          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           target="_blank"
           rel="noopener noreferrer"
@@ -61,58 +107,82 @@ export default function Home() {
             Find in-depth information about Next.js features and API.
           </p>
         </a>
+      </div> */}
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+      <nav style= {{fontSize:"20px"}}>
+        <li style={{fontSize:"30px"}}>
+          <Image 
+          style={{display:"inline"}} 
+          src="/image 48.png" 
+          alt="Pickaxe logo" 
+          width={32} 
+          height={32} 
+          priority/>
+          {" "} Pickaxe
+          </li>
+        <li>Make A Pickaxe</li>
+        <li>Dashboard</li>
+        <li>Enterprise</li>
+        <li>Tools</li>
+        <li style={{alignContent: "right"}}>UserName</li>
+      </nav>
+      <div className="grid grid-flow-col w-full h-full justify-stretch min-w-900">
+      <div className="flex flex-col min-w-450 justify-center content-center justify-items-center">
+            <Image
+              src="/chromewalnut_robot_fairy_composing_minimalist_figuration_single_682591b0-72bf-4eb4-be8d-83b3664393da.png"
+              alt="robot fairy Logo"
+              className=""
+              style={{alignSelf:"center"}}
+              width={318}
+              height={318}
+              priority
+            />
+            <div style={{textAlign:"center", fontSize:"24px"}}>
+            <p >Create your own AI App</p>
+              <p>with our Wizard</p>
+            </div>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
+        </div>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div className="flex flex-col min-w-450 bg-white justify-center" style={{paddingLeft:"3%", font: "black"}}>
+      <p className="decoration-double" style={{fontSize: "20px"}}>Step 1</p>
+      <p>Turn your idea into an prompt</p>
+
+    <input style={{ border: "black", borderStyle: "solid", minWidth:"550px", minHeight: "100px", font: "black"}}
+            placeholder='In a phrase or two, describe what you want your bot to be good at.'
+            className=' textarea w-full max-w-xs input input-bordered input-secondary'
+            value={value}
+            onChange={handleInput}
+            onKeyDown={handleKeyDown}
+          />
+          <button
+            className='btn btn-primary btn-xs mt-6'
+            onClick={handleRefresh}
+          >
+            Get your prompt
+          </button>
+
+    <div className={hide} style={{marginTop:"20px"}}>
+    <p className="decoration-double" style={{fontSize: "20px"}}>Step 2</p>
+      <p>Look over your prompt, polish it, and turn it into an app</p>
+      <input style={{ border: "black", borderStyle: "solid", minWidth:"550px", minHeight: "100px", font: "black"}}
+            className='textarea w-full max-w-xs input input-bordered input-secondary'
+            value={prompter}
+            onChange={handleInput2}
+          />
+          <button
+            className='btn btn-primary btn-xs mt-6'
+            onClick={handleKeyDown2}
+          >
+            Build your app
+          </button>
+          </div>
+
+
+
       </div>
+      </div>
+
     </main>
   )
 }
