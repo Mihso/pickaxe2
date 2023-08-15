c87f91b084df98e42d31f603df12f49e7f881110
Matthew Oshimo <moshimo90696@gmail.com>
2023-08-15 02:30:34 -0700

    more adjustments

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,16 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import localFont from 'next/font/local'
+
+
+const myFont = localFont({ src: [
+  {path: './fonts/RealHeadPro-Bold.otf'},
+  {path:'./fonts/RealHeadPro-Demibold.otf'},
+  {path:'./fonts/RealHeadPro-Regular.otf'},
+  {path:'./fonts/RealHeadPro-SemiLight.otf'}] })
 
 export default function App({ Component, pageProps }: AppProps) {
+
   return <Component {...pageProps} />
+
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,6 @@
 'use client'
-
 import Image from 'next/image'
-import React, { useRef } from 'react'
+import React from 'react'
 
 interface Conversation {
   role: string
@@ -14,24 +13,10 @@ export default function Home() {
   const [hide, setHide] = React.useState<string>("hidden")
   const [prompter, setPrompter] = React.useState<string>("")
   const [conversation, setConversation] = React.useState<Conversation[]>([])
-  const inputRef = useRef<HTMLInputElement>(null)
 
-  const handleInput = React.useCallback(
-    (e: React.ChangeEvent<HTMLInputElement>) => {
-      setValue(e.target.value)
-    },
-    []
-  )
-  const handleInput2 = React.useCallback(
-    (e: React.ChangeEvent<HTMLInputElement>) => {
-      setPrompter(e.target.value)
-    },[]
-  )
-
-
-  const handleKeyDown = async (e: React.KeyboardEvent<HTMLInputElement>) => {
-    if (e.key === "Enter") {
-      const chatHistory = [...conversation, { role: "user", content: 'Based on the word "' + value + '", give me a prompt for you that clarifies in at least 3 sentences: Role, Style, and Goal.' }]
+  const handleKeyDown = async () => {
+      setHide("hidden")
+      const chatHistory = [...conversation, { role: "user", content: 'Based on the keywords "' + value + '", give me a one paragraph prompt for you that at least answers role, style, and goal.' }]
 
       const response = await fetch("/api/OpenAI", {
         method: "POST",
@@ -53,12 +38,11 @@ export default function Home() {
 
 
       setHide("")
-    }
   }
 
   const handleKeyDown2 = async () => {
       const chatHistory = [...conversation, { role: "user", content: 'Behave in a manner based on this prompt: "' + value + '".'} ]
- 
+      console.log(prompter)
       const response = await fetch("/api/OpenAI", {
         method: "POST",
         headers: {
@@ -79,37 +63,39 @@ export default function Home() {
       setHide("")
   }
 
-  const handleRefresh = () => {
-    inputRef.current?.focus()
-    console.log("pressed")
-    setValue("")
-    setConversation([])
+  function step2(){
+
+    if(hide !=="hidden"){
+      return (
+        <div className={hide} style={{marginTop:"20px"}}>
+        <p className="decoration-double" style={{fontSize: "20px"}}>Step 2</p>
+          <p>Look over your prompt, polish it, and turn it into an app</p>
+          <textarea style={{ border: "black", borderStyle: "solid", width: "100%", minHeight: "100px", font: "black"}}
+                className='textarea w-full max-w-xs input input-bordered'
+                defaultValue={prompter}
+                onChange = {(e)=>setPrompter(e.target.value)}
+              />
+              <button
+                onClick={handleKeyDown2}
+              >
+                Build your app
+              </button>
+              </div>
+      )
+
+    }
+    else{
+    return(
+      <></>
+    )
+    }
   }
 
   return (
 
     <main className="flex h-screen min-w-900 flex-col content-start">
 
-      {/* <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-      </div> */}
-
-      <nav style= {{fontSize:"20px"}}>
+      <nav style= {{fontSize:"18px"}}>
         <li style={{fontSize:"30px"}}>
           <Image 
           style={{display:"inline"}} 
@@ -124,7 +110,28 @@ export default function Home() {
         <li>Dashboard</li>
         <li>Enterprise</li>
         <li>Tools</li>
-        <li style={{alignContent: "right"}}>UserName</li>
+        <li className='absolute right-0'> <Image 
+          style={{display:"inline"}} 
+          src="/search.png" 
+          alt="Search logo" 
+          width={24} 
+          height={24} 
+          priority/>
+          {" "}
+          <Image 
+          style={{display:"inline"}} 
+          src="/user.png" 
+          alt="user logo" 
+          width={24} 
+          height={24} 
+          priority/>
+          {" "}UserName{" "}<Image 
+          style={{display:"inline"}} 
+          src="/menu.png" 
+          alt="Menu logo" 
+          width={24} 
+          height={24} 
+          priority/></li>
       </nav>
       <div className="grid grid-flow-col w-full h-full justify-stretch min-w-900">
       <div className="flex flex-col min-w-450 justify-center content-center justify-items-center">
@@ -148,37 +155,19 @@ export default function Home() {
       <p className="decoration-double" style={{fontSize: "20px"}}>Step 1</p>
       <p>Turn your idea into an prompt</p>
 
-    <input style={{ border: "black", borderStyle: "solid", minWidth:"550px", minHeight: "100px", font: "black"}}
+    <textarea style={{ border: "black", borderStyle: "solid", width:"100%", minHeight: "100px", font: "black"}}
             placeholder='In a phrase or two, describe what you want your bot to be good at.'
             className=' textarea w-full max-w-xs input input-bordered input-secondary'
             value={value}
-            onChange={handleInput}
-            onKeyDown={handleKeyDown}
+            onChange={(e)=>setValue(e.target.value)}
           />
           <button
-            className='btn btn-primary btn-xs mt-6'
-            onClick={handleRefresh}
+            onClick={handleKeyDown}
           >
             Get your prompt
           </button>
 
-    <div className={hide} style={{marginTop:"20px"}}>
-    <p className="decoration-double" style={{fontSize: "20px"}}>Step 2</p>
-      <p>Look over your prompt, polish it, and turn it into an app</p>
-      <input style={{ border: "black", borderStyle: "solid", minWidth:"550px", minHeight: "100px", font: "black"}}
-            className='textarea w-full max-w-xs input input-bordered input-secondary'
-            value={prompter}
-            onChange={handleInput2}
-          />
-          <button
-            className='btn btn-primary btn-xs mt-6'
-            onClick={handleKeyDown2}
-          >
-            Build your app
-          </button>
-          </div>
-
-
+      {step2()}
 
       </div>
       </div>
